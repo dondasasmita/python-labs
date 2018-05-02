2c959f959160d2a84c6a3b267a5c08d53f9cfca7
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-02 19:35:30 +0800

    string log message

---
+++ 03-server-log-message.py
@@ -0,0 +1,11 @@
+# TODO: print a log message using the variables above.
+# The message should have the same format as this one:
+# "Yogesh accessed the site http://petshop.com/pets/reptiles/pythons at 16:20."
+
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+
+# Solution: 
+print(username + " accessed the site " + url + " at " + timestamp + " .")
+
