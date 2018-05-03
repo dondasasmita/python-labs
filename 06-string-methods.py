6a8e0e550ceef67aa0c44cd218bc405b5710bd58
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-03 13:46:11 +0800

    built-in String methods in python

---
+++ 06-string-methods.py
@@ -0,0 +1,14 @@
+# Browse the complete list of string methods at:
+# https://docs.python.org/3/library/stdtypes.html#string-methods
+# and try them out here
+
+first_name = 'donda'
+last_name = 'sasmita'
+
+# print Donda Sasmita
+print(first_name.capitalize() + " " + last_name.capitalize())
+
+
+
+
+
