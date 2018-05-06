611370a43242168eb13d323955b221a6cf6de593
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-06 10:20:42 +0700

    lists method sort and join

---
+++ 08-lists-methods.py
@@ -0,0 +1,12 @@
+# sort method in an int list will return the lowest number to the highest
+numbers = [15, 30, 31, 1, 7, 84, 81]
+numbers.sort()
+print(numbers)
+
+# join method will return the values of the list joined by the values that was passed in
+idiom = ["better", "late", "than", "never"]
+print(idiom)
+print()
+print("\n".join(idiom))
+print()
+print("-".join(idiom))
